594dbe61d823801233691f07429e2c96e9258578
Azat Ibrakov <azatibrakov@gmail.com>
2020-02-21 10:38:44 +0300

    Complete strategies

--- tests/base_tests/strategies.py
+++ tests/base_tests/strategies.py
@@ -12,19 +12,22 @@ from tests.utils import (Strategy,
 
 contours_strategies = scalars_strategies.map(planar.contours)
 
+empty_multipolygons = strategies.builds(list)
+
 
 def to_simple_multipolygons(contours: Strategy[Contour]
                             ) -> Strategy[Multipolygon]:
     def to_simple_multipolygon(polygon: Polygon) -> Multipolygon:
         return [polygon]
 
-    return (strategies.tuples(contours, strategies.builds(list))
-            .map(to_simple_multipolygon))
+    return (empty_multipolygons
+            | (strategies.tuples(contours, empty_multipolygons)
+               .map(to_simple_multipolygon)))
 
 
 multipolygons_strategies = contours_strategies.map(to_simple_multipolygons)
 multipolygons = multipolygons_strategies.flatmap(identity)
-empty_multipolygons_with_multipolygons = strategies.tuples(
-        strategies.builds(list), multipolygons)
+empty_multipolygons_with_multipolygons = strategies.tuples(empty_multipolygons,
+                                                           multipolygons)
 multipolygons_pairs = multipolygons_strategies.flatmap(to_pairs)
 multipolygons_triplets = multipolygons_strategies.flatmap(to_triplets)
