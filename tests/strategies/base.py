e3d475ee81c324c8722a7a33440c932d30b239b8
Azat Ibrakov <azatibrakov@gmail.com>
2020-02-25 14:16:27 +0300

    Complete strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -3,14 +3,14 @@ from functools import partial
 
 from hypothesis import strategies
 
-MAX_FLOAT = 10 ** 10
-MIN_FLOAT = -MAX_FLOAT
+MAX_NUMBER = 10 ** 10
+MIN_NUMBER = -MAX_NUMBER
 scalars_strategies_factories = {float: partial(strategies.floats,
-                                               MIN_FLOAT, MAX_FLOAT,
                                                allow_nan=False,
                                                allow_infinity=False),
-                                Fraction: strategies.fractions,
+                                Fraction: partial(strategies.fractions,
+                                                  max_denominator=MAX_NUMBER),
                                 int: strategies.integers}
 scalars_strategies = strategies.sampled_from(
-        [factory()
+        [factory(MIN_NUMBER, MAX_NUMBER)
          for factory in scalars_strategies_factories.values()])
