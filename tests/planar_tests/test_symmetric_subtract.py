d53893f4f50ed96d460e1057b53f276fefe6ade6
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-02 19:24:12 +0700

    Complete test for 'symmetric_subtract' function

--- tests/planar_tests/test_symmetric_subtract.py
+++ tests/planar_tests/test_symmetric_subtract.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from hypothesis import given
 
 from clipping.planar import (intersect,
@@ -12,29 +14,32 @@ from tests.utils import (Multipolygon,
                          reverse_multipolygon,
                          reverse_multipolygon_borders,
                          reverse_multipolygon_holes,
-                         reverse_multipolygon_holes_contours)
+                         reverse_multipolygon_holes_contours,
+                         rotate_sequence)
 from . import strategies
 
 
-@given(strategies.multipolygons_pairs)
-def test_basic(multipolygons_pair: MultipolygonsPair) -> None:
-    left_multipolygon, right_multipolygon = multipolygons_pair
-
-    result = symmetric_subtract(left_multipolygon, right_multipolygon)
+@given(strategies.multipolygons_lists)
+def test_basic(multipolygons: List[Multipolygon]) -> None:
+    result = symmetric_subtract(*multipolygons)
 
     assert is_multipolygon(result)
 
 
-@given(strategies.multipolygons)
-def test_self_inverse(multipolygon: Multipolygon) -> None:
-    result = symmetric_subtract(multipolygon, multipolygon)
+@given(strategies.empty_multipolygons_lists)
+def test_degenerate(multipolygons: List[Multipolygon]) -> None:
+    assert symmetric_subtract(*multipolygons) == []
 
-    assert not result
+
+@given(strategies.multipolygons)
+def test_self(multipolygon: Multipolygon) -> None:
+    assert symmetric_subtract(multipolygon) == multipolygon
+    assert symmetric_subtract(multipolygon, multipolygon) == []
 
 
 @given(strategies.empty_multipolygons_with_multipolygons)
-def test_left_neutral_element(
-        empty_multipolygon_with_multipolygon: MultipolygonsPair) -> None:
+def test_left_neutral_element(empty_multipolygon_with_multipolygon
+                              : MultipolygonsPair) -> None:
     empty_multipolygon, multipolygon = empty_multipolygon_with_multipolygon
 
     result = symmetric_subtract(empty_multipolygon, multipolygon)
@@ -43,57 +48,13 @@ def test_left_neutral_element(
 
 
 @given(strategies.empty_multipolygons_with_multipolygons)
-def test_right_neutral_element(
-        empty_multipolygon_with_multipolygon: MultipolygonsPair) -> None:
+def test_right_neutral_element(empty_multipolygon_with_multipolygon
+                               : MultipolygonsPair) -> None:
     empty_multipolygon, multipolygon = empty_multipolygon_with_multipolygon
 
     result = symmetric_subtract(multipolygon, empty_multipolygon)
 
     assert are_multipolygons_similar(result, multipolygon)
-
-
-@given(strategies.multipolygons_pairs)
-def test_commutativity(multipolygons_pair: MultipolygonsPair) -> None:
-    left_multipolygon, right_multipolygon = multipolygons_pair
-
-    result = symmetric_subtract(left_multipolygon, right_multipolygon)
-
-    assert result == symmetric_subtract(right_multipolygon, left_multipolygon)
-
-
-@given(strategies.multipolygons_pairs)
-def test_equivalent_using_union_of_differences(
-        multipolygons_pair: MultipolygonsPair) -> None:
-    left_multipolygon, right_multipolygon = multipolygons_pair
-
-    result = symmetric_subtract(left_multipolygon, right_multipolygon)
-
-    assert result == unite(subtract(left_multipolygon, right_multipolygon),
-                           subtract(right_multipolygon, left_multipolygon))
-
-
-@given(strategies.multipolygons_pairs)
-def test_equivalent_using_difference_of_union_and_intersection(
-        multipolygons_pair: MultipolygonsPair) -> None:
-    left_multipolygon, right_multipolygon = multipolygons_pair
-
-    result = symmetric_subtract(left_multipolygon, right_multipolygon)
-
-    assert result == subtract(unite(left_multipolygon, right_multipolygon),
-                              intersect(right_multipolygon, left_multipolygon))
-
-
-@given(strategies.multipolygons_pairs)
-def test_expressing_union_as_symmetric_difference(
-        multipolygons_pair: MultipolygonsPair) -> None:
-    left_multipolygon, right_multipolygon = multipolygons_pair
-
-    result = symmetric_subtract(symmetric_subtract(left_multipolygon,
-                                                   right_multipolygon),
-                                intersect(left_multipolygon,
-                                          right_multipolygon))
-
-    assert result == unite(left_multipolygon, right_multipolygon)
 
 
 @given(strategies.multipolygons_triplets)
@@ -127,39 +88,50 @@ def test_repeated(multipolygons_triplet: MultipolygonsTriplet) -> None:
 
 
 @given(strategies.multipolygons_pairs)
-def test_reversals(multipolygons_pair: MultipolygonsPair) -> None:
+def test_equivalents(multipolygons_pair: MultipolygonsPair) -> None:
     left_multipolygon, right_multipolygon = multipolygons_pair
 
     result = symmetric_subtract(left_multipolygon, right_multipolygon)
 
-    assert are_multipolygons_similar(
-            result, symmetric_subtract(reverse_multipolygon(left_multipolygon),
-                                       right_multipolygon))
-    assert are_multipolygons_similar(
-            result,
-            symmetric_subtract(left_multipolygon,
-                               reverse_multipolygon(right_multipolygon)))
-    assert are_multipolygons_similar(
-            result,
-            symmetric_subtract(reverse_multipolygon_borders(left_multipolygon),
-                               right_multipolygon))
-    assert are_multipolygons_similar(
-            result, symmetric_subtract(
-                    left_multipolygon,
-                    reverse_multipolygon_borders(right_multipolygon)))
+    assert result == unite(subtract(left_multipolygon, right_multipolygon),
+                           subtract(right_multipolygon, left_multipolygon))
+    assert result == subtract(unite(left_multipolygon, right_multipolygon),
+                              intersect(right_multipolygon, left_multipolygon))
+
+
+@given(strategies.non_empty_multipolygons_lists)
+def test_reversals(multipolygons: List[Multipolygon]) -> None:
+    first_multipolygon, *rest_multipolygons = multipolygons
+
+    result = symmetric_subtract(first_multipolygon, *rest_multipolygons)
+
     assert are_multipolygons_similar(
             result,
-            symmetric_subtract(reverse_multipolygon_holes(left_multipolygon),
-                               right_multipolygon))
+            symmetric_subtract(reverse_multipolygon(first_multipolygon),
+                               *rest_multipolygons))
     assert are_multipolygons_similar(
-            result, symmetric_subtract(
-                    left_multipolygon,
-                    reverse_multipolygon_holes(right_multipolygon)))
+            result,
+            symmetric_subtract(
+                    reverse_multipolygon_borders(first_multipolygon),
+                    *rest_multipolygons))
     assert are_multipolygons_similar(
-            result, symmetric_subtract(
-                    reverse_multipolygon_holes_contours(left_multipolygon),
-                    right_multipolygon))
+            result,
+            symmetric_subtract(reverse_multipolygon_holes(first_multipolygon),
+                               *rest_multipolygons))
     assert are_multipolygons_similar(
-            result, symmetric_subtract(
-                    left_multipolygon,
-                    reverse_multipolygon_holes_contours(right_multipolygon)))
+            result,
+            symmetric_subtract(
+                    reverse_multipolygon_holes_contours(first_multipolygon),
+                    *rest_multipolygons))
+
+
+@given(strategies.multipolygons_lists)
+def test_rotations(multipolygons: List[Multipolygon]) -> None:
+    result = symmetric_subtract(*multipolygons)
+
+    assert all(
+            are_multipolygons_similar(result,
+                                      symmetric_subtract(
+                                              *rotate_sequence(multipolygons,
+                                                               offset)))
+            for offset in range(1, len(multipolygons)))
