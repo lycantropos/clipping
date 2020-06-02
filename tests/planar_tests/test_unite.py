92d6759d1d9ef9768f42b6e4abe0ffd220e6463a
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-02 19:27:02 +0700

    Complete test for 'unite' function

--- tests/planar_tests/test_unite.py
+++ tests/planar_tests/test_unite.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from hypothesis import given
 
 from clipping.hints import Multipolygon
 from clipping.planar import (intersect,
                              subtract,
+                             symmetric_subtract,
                              unite)
 from tests.utils import (MultipolygonsPair,
                          MultipolygonsTriplet,
@@ -11,29 +14,33 @@ from tests.utils import (MultipolygonsPair,
                          reverse_multipolygon,
                          reverse_multipolygon_borders,
                          reverse_multipolygon_holes,
-                         reverse_multipolygon_holes_contours)
+                         reverse_multipolygon_holes_contours,
+                         rotate_sequence)
 from . import strategies
 
 
-@given(strategies.multipolygons_pairs)
-def test_basic(multipolygons_pair: MultipolygonsPair) -> None:
-    left_multipolygon, right_multipolygon = multipolygons_pair
-
-    result = unite(left_multipolygon, right_multipolygon)
+@given(strategies.multipolygons_lists)
+def test_basic(multipolygons: List[Multipolygon]) -> None:
+    result = unite(*multipolygons)
 
     assert is_multipolygon(result)
 
 
-@given(strategies.multipolygons)
-def test_idempotence(multipolygon: Multipolygon) -> None:
-    result = unite(multipolygon, multipolygon)
+@given(strategies.empty_multipolygons_lists)
+def test_degenerate(multipolygons: List[Multipolygon]) -> None:
+    assert unite(*multipolygons) == []
 
-    assert are_multipolygons_similar(result, multipolygon)
+
+@given(strategies.multipolygons)
+def test_self(multipolygon: Multipolygon) -> None:
+    assert unite(multipolygon) == multipolygon
+    assert are_multipolygons_similar(unite(multipolygon, multipolygon),
+                                     multipolygon)
 
 
 @given(strategies.empty_multipolygons_with_multipolygons)
-def test_left_neutral_element(
-        empty_multipolygon_with_multipolygon: MultipolygonsPair) -> None:
+def test_left_neutral_element(empty_multipolygon_with_multipolygon
+                              : MultipolygonsPair) -> None:
     empty_multipolygon, multipolygon = empty_multipolygon_with_multipolygon
 
     result = unite(empty_multipolygon, multipolygon)
@@ -42,8 +49,8 @@ def test_left_neutral_element(
 
 
 @given(strategies.empty_multipolygons_with_multipolygons)
-def test_right_neutral_element(
-        empty_multipolygon_with_multipolygon: MultipolygonsPair) -> None:
+def test_right_neutral_element(empty_multipolygon_with_multipolygon
+                               : MultipolygonsPair) -> None:
     empty_multipolygon, multipolygon = empty_multipolygon_with_multipolygon
 
     result = unite(multipolygon, empty_multipolygon)
@@ -101,8 +108,8 @@ def test_difference_operand(multipolygons_triplet: MultipolygonsTriplet
 
 
 @given(strategies.multipolygons_triplets)
-def test_distribution_over_intersection(
-        multipolygons_triplet: MultipolygonsTriplet) -> None:
+def test_distribution_over_intersection(multipolygons_triplet
+                                        : MultipolygonsTriplet) -> None:
     (left_multipolygon, mid_multipolygon,
      right_multipolygon) = multipolygons_triplet
 
@@ -117,36 +124,43 @@ def test_distribution_over_intersection(
 
 
 @given(strategies.multipolygons_pairs)
-def test_reversals(multipolygons_pair: MultipolygonsPair) -> None:
+def test_equivalents(multipolygons_pair: MultipolygonsPair) -> None:
     left_multipolygon, right_multipolygon = multipolygons_pair
 
     result = unite(left_multipolygon, right_multipolygon)
 
+    assert result == symmetric_subtract(symmetric_subtract(left_multipolygon,
+                                                           right_multipolygon),
+                                        intersect(left_multipolygon,
+                                                  right_multipolygon))
+
+
+@given(strategies.non_empty_multipolygons_lists)
+def test_reversals(multipolygons: List[Multipolygon]) -> None:
+    first_multipolygon, *rest_multipolygons = multipolygons
+
+    result = unite(first_multipolygon, *rest_multipolygons)
+
     assert are_multipolygons_similar(
-            result, unite(reverse_multipolygon(left_multipolygon),
-                          right_multipolygon))
+            result, unite(reverse_multipolygon(first_multipolygon),
+                          *rest_multipolygons))
     assert are_multipolygons_similar(
-            result, unite(left_multipolygon,
-                          reverse_multipolygon(right_multipolygon)))
+            result, unite(reverse_multipolygon_borders(first_multipolygon),
+                          *rest_multipolygons))
+    assert are_multipolygons_similar(
+            result, unite(reverse_multipolygon_holes(first_multipolygon),
+                          *rest_multipolygons))
     assert are_multipolygons_similar(
             result,
-            unite(reverse_multipolygon_borders(left_multipolygon),
-                  right_multipolygon))
-    assert are_multipolygons_similar(
-            result, unite(
-                    left_multipolygon,
-                    reverse_multipolygon_borders(right_multipolygon)))
-    assert are_multipolygons_similar(
-            result, unite(reverse_multipolygon_holes(left_multipolygon),
-                          right_multipolygon))
-    assert are_multipolygons_similar(
-            result, unite(left_multipolygon,
-                          reverse_multipolygon_holes(right_multipolygon)))
-    assert are_multipolygons_similar(
-            result, unite(
-                    reverse_multipolygon_holes_contours(left_multipolygon),
-                    right_multipolygon))
-    assert are_multipolygons_similar(
-            result, unite(
-                    left_multipolygon,
-                    reverse_multipolygon_holes_contours(right_multipolygon)))
+            unite(reverse_multipolygon_holes_contours(first_multipolygon),
+                  *rest_multipolygons))
+
+
+@given(strategies.multipolygons_lists)
+def test_rotations(multipolygons: List[Multipolygon]) -> None:
+    result = unite(*multipolygons)
+
+    assert all(are_multipolygons_similar(result,
+                                         unite(*rotate_sequence(multipolygons,
+                                                                offset)))
+               for offset in range(1, len(multipolygons)))
