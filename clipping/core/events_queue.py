38f115466159017a099719b6538c0d5baeeee013
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-10 21:47:03 +0700

    Complete 'EventsQueueKey.__lt__' magic method

--- clipping/core/events_queue.py
+++ clipping/core/events_queue.py
@@ -43,12 +43,8 @@ class EventsQueueKey:
             # the lowest segment is processed first
             if other_end_orientation is not Orientation.COLLINEAR:
                 return other_end_orientation is Orientation.COUNTERCLOCKWISE
-            elif event.from_left is not other_event.from_left:
-                return event.from_left
             else:
-                end_x, end_y = event.end
-                other_end_x, other_end_y = other_event.end
-                return end_y < other_end_y
+                return event.from_left
 
 
 EventsQueue = cast(Callable[[], PriorityQueue[Event]],
