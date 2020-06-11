efbd002248115cc2abdc8b2f64213714c6720449
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-11 20:13:24 +0700

    Add test for 'touches_with' function

---
+++ tests/bounding_box_tests/test_touches_with.py
@@ -0,0 +1,42 @@
+from hypothesis import given
+from orient.planar import (Relation,
+                           region_in_region)
+
+from clipping.core.bounding_box import (to_vertices,
+                                        touches_with)
+from clipping.hints import BoundingBox
+from tests.utils import BoundingBoxesPair
+from . import strategies
+
+
+@given(strategies.bounding_boxes_pairs)
+def test_basic(bounding_boxes_pair: BoundingBoxesPair) -> None:
+    left_bounding_box, right_bounding_box = bounding_boxes_pair
+
+    result = touches_with(left_bounding_box, right_bounding_box)
+
+    assert isinstance(result, bool)
+
+
+@given(strategies.bounding_boxes)
+def test_irreflexivity(bounding_box: BoundingBox) -> None:
+    assert not touches_with(bounding_box, bounding_box)
+
+
+@given(strategies.bounding_boxes_pairs)
+def test_symmetry(bounding_boxes_pair: BoundingBoxesPair) -> None:
+    left_bounding_box, right_bounding_box = bounding_boxes_pair
+
+    assert (touches_with(left_bounding_box, right_bounding_box)
+            is touches_with(right_bounding_box, left_bounding_box))
+
+
+@given(strategies.bounding_boxes_pairs)
+def test_equivalents(bounding_boxes_pair: BoundingBoxesPair) -> None:
+    left_bounding_box, right_bounding_box = bounding_boxes_pair
+
+    result = touches_with(left_bounding_box, right_bounding_box)
+
+    assert result is (region_in_region(to_vertices(left_bounding_box),
+                                       to_vertices(right_bounding_box))
+                      is Relation.TOUCH)
