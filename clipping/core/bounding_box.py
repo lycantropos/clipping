886ac93392bf76a11420c88d1b0f01648a3dee2c
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-11 18:44:33 +0700

    Complete 'touches_with' function

--- clipping/core/bounding_box.py
+++ clipping/core/bounding_box.py
@@ -72,7 +72,9 @@ def touches_with(left: BoundingBox, right: BoundingBox) -> bool:
     left_x_min, left_x_max, left_y_min, left_y_max = left
     right_x_min, right_x_max, right_y_min, right_y_max = right
     return ((left_x_min == right_x_max or left_x_max == right_x_min)
-            and (left_y_min == right_y_max or left_y_max == right_y_min))
+            and (left_y_min <= right_y_max and right_y_min <= left_y_max)
+            or (left_x_min <= right_x_max and right_x_min <= left_x_max)
+            and (left_y_min == right_y_max or right_y_min == left_y_max))
 
 
 def is_subset_of(test: BoundingBox, goal: BoundingBox) -> bool:
