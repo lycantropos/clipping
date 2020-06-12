641a994b9b5f94c86127b6d0ffa58ed774a2c5dd
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-12 23:42:22 +0700

    Add test for 'is_subset_of_region' function

--- tests/bounding_box_tests/strategies.py
+++ tests/bounding_box_tests/strategies.py
@@ -1,10 +1,27 @@
+from typing import Tuple
+
+from hypothesis import strategies
 from hypothesis_geometry import planar
 
+from clipping.hints import (BoundingBox,
+                            Contour,
+                            Coordinate)
 from tests.strategies import coordinates_strategies
-from tests.utils import (to_pairs,
+from tests.utils import (Strategy,
+                         to_pairs,
                          to_triplets)
 
 bounding_boxes = coordinates_strategies.flatmap(planar.bounding_boxes)
+
+
+def to_contours_with_bounding_boxes(coordinates: Strategy[Coordinate]
+                                    ) -> Strategy[Tuple[Contour, BoundingBox]]:
+    return strategies.tuples(planar.contours(coordinates),
+                             planar.bounding_boxes(coordinates))
+
+
+contours_with_bounding_boxes = (coordinates_strategies
+                                .flatmap(to_contours_with_bounding_boxes))
 bounding_boxes_strategies = coordinates_strategies.map(planar.bounding_boxes)
 bounding_boxes_pairs = bounding_boxes_strategies.flatmap(to_pairs)
 bounding_boxes_triplets = bounding_boxes_strategies.flatmap(to_triplets)
---
+++ tests/bounding_box_tests/test_is_subset_of_region.py
@@ -0,0 +1,38 @@
+from typing import Tuple
+
+from hypothesis import given
+from orient.planar import (Relation,
+                           region_in_region)
+
+from clipping.core.bounding_box import (is_subset_of_region,
+                                        to_vertices)
+from clipping.hints import (BoundingBox,
+                            Contour)
+from . import strategies
+
+
+@given(strategies.contours_with_bounding_boxes)
+def test_basic(contour_with_bounding_box: Tuple[Contour, BoundingBox]) -> None:
+    contour, bounding_box = contour_with_bounding_box
+
+    result = is_subset_of_region(bounding_box, contour)
+
+    assert isinstance(result, bool)
+
+
+@given(strategies.bounding_boxes)
+def test_self(bounding_box: BoundingBox) -> None:
+    assert is_subset_of_region(bounding_box, list(to_vertices(bounding_box)))
+
+
+@given(strategies.contours_with_bounding_boxes)
+def test_equivalents(contour_with_bounding_box: Tuple[Contour, BoundingBox]
+                     ) -> None:
+    contour, bounding_box = contour_with_bounding_box
+
+    result = is_subset_of_region(bounding_box, contour)
+
+    assert result is (region_in_region(to_vertices(bounding_box), contour)
+                      in (Relation.EQUAL,
+                          Relation.ENCLOSED,
+                          Relation.WITHIN))
