8d694011b1ec5dacd0290c3a9f4b2c5295e9a0a1
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-13 16:02:07 +0700

    Complete test for 'overlaps_with_polygon' function

--- tests/bounding_box_tests/test_overlaps_with_polygon.py
+++ tests/bounding_box_tests/test_overlaps_with_polygon.py
@@ -4,7 +4,9 @@ from hypothesis import given
 from orient.planar import (Relation,
                            region_in_polygon)
 
-from clipping.core.bounding_box import (overlaps_with_polygon,
+from clipping.core.bounding_box import (from_points,
+                                        is_subset_of_region,
+                                        overlaps_with_polygon,
                                         to_vertices)
 from clipping.hints import (BoundingBox,
                             Polygon)
@@ -20,10 +22,17 @@ def test_basic(polygon_with_bounding_box: Tuple[Polygon, BoundingBox]) -> None:
     assert isinstance(result, bool)
 
 
-@given(strategies.bounding_boxes)
-def test_self(bounding_box: BoundingBox) -> None:
+@given(strategies.polygons_with_bounding_boxes)
+def test_self(polygon_with_bounding_box: Tuple[Polygon, BoundingBox]) -> None:
+    polygon, bounding_box = polygon_with_bounding_box
+
+    border, holes = polygon
     assert overlaps_with_polygon(bounding_box,
                                  (list(to_vertices(bounding_box)), []))
+    assert overlaps_with_polygon(from_points(border), polygon)
+    assert all(overlaps_with_polygon(from_points(hole), polygon)
+               or is_subset_of_region(from_points(hole), hole)
+               for hole in holes)
 
 
 @given(strategies.polygons_with_bounding_boxes)
