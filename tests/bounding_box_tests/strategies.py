8f79715574a9ba628c83fb67e0192ff455520efb
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-27 11:38:57 +0300

    Complete strategies

--- tests/bounding_box_tests/strategies.py
+++ tests/bounding_box_tests/strategies.py
@@ -12,7 +12,27 @@ from tests.utils import (Strategy,
                          to_pairs,
                          to_triplets)
 
-bounding_boxes = coordinates_strategies.flatmap(planar.bounding_boxes)
+
+def to_degenerate_bounding_boxes(coordinates: Strategy[Coordinate]
+                                 ) -> Strategy[BoundingBox]:
+    def to_x_degenerate_bounding_box(x: Coordinate,
+                                     y_min: Coordinate,
+                                     y_max: Coordinate) -> BoundingBox:
+        return x, x, y_min, y_max
+
+    def to_y_degenerate_bounding_box(x_min: Coordinate,
+                                     x_max: Coordinate,
+                                     y: Coordinate) -> BoundingBox:
+        return y, y, x_min, x_max
+
+    return (strategies.builds(to_x_degenerate_bounding_box, coordinates,
+                              coordinates, coordinates)
+            | strategies.builds(to_y_degenerate_bounding_box, coordinates,
+                                coordinates, coordinates))
+
+
+bounding_boxes = (coordinates_strategies.flatmap(to_degenerate_bounding_boxes)
+                  | coordinates_strategies.flatmap(planar.bounding_boxes))
 
 
 def to_contours_with_bounding_boxes(coordinates: Strategy[Coordinate]
