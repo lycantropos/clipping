7610169d388c4cd7188b62d8312b3d45a21fb09a
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-28 00:36:21 +0700

    Remove unused functions

--- clipping/core/bounding_box.py
+++ clipping/core/bounding_box.py
@@ -108,24 +108,6 @@ def coupled_with(left: BoundingBox, right: BoundingBox) -> bool:
                  or edges_overlap_with(left, right)))
 
 
-def to_diagonal(bounding_box: BoundingBox) -> Segment:
-    x_min, x_max, y_min, y_max = bounding_box
-    return (x_min, y_min), (x_max, y_max)
-
-
-def is_degenerate(box: BoundingBox) -> bool:
-    """
-    Checks if bounding box is degenerate.
-
-    >>> is_degenerate((0, 2, 0, 2))
-    False
-    >>> is_degenerate((0, 0, 0, 2))
-    True
-    """
-    x_min, x_max, y_min, y_max = box
-    return x_min == x_max or y_min == y_max
-
-
 def touches_with(left: BoundingBox, right: BoundingBox) -> bool:
     """
     Checks if bounding boxes intersect at point or by the edge.
