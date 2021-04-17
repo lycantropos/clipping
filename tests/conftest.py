abbaacf68209466d0f1ebc6001a86064d250befb
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-17 10:39:05 +0300

    Complete tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,6 @@
 import os
 import platform
 
-import pytest
-from ground.base import (Context,
-                         get_context)
 from hypothesis import (HealthCheck,
                         settings)
 
@@ -17,8 +14,3 @@ settings.register_profile('default',
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
-
-
-@pytest.fixture(scope='session')
-def context() -> Context:
-    return get_context()
