ab29afe08cd058215f4564e818a4c4182bd3da32
Azat Ibrakov <azatibrakov@gmail.com>
2021-05-12 13:03:22 +0300

    Complete test for 'subtract_multipolygon_from_multisegment' function

--- tests/planar_tests/test_subtract_multipolygon_from_multisegment.py
+++ tests/planar_tests/test_subtract_multipolygon_from_multisegment.py
@@ -8,6 +8,7 @@ from clipping.planar import subtract_multipolygon_from_multisegment
 from tests.utils import (MultipolygonWithMultisegment,
                          are_compounds_similar,
                          is_maybe_linear,
+                         pack_non_shaped,
                          reverse_compound_coordinates,
                          reverse_multipolygon,
                          reverse_multipolygon_borders,
@@ -40,18 +41,20 @@ def test_properties(multipolygon_with_multisegment
     result = subtract_multipolygon_from_multisegment(multisegment,
                                                      multipolygon)
 
+    result_points, result_segments = pack_non_shaped(result)
+    assert not result_points
     assert all(segment_in_multisegment(segment, multisegment)
                in (Relation.EQUAL, Relation.COMPONENT)
-               for segment in result.segments)
+               for segment in result_segments)
     assert all(segment_in_multipolygon(segment, multipolygon)
                in (Relation.DISJOINT, Relation.TOUCH)
-               for segment in result.segments)
-    assert all(segment in result.segments
-               or reverse_segment(segment) in result.segments
+               for segment in result_segments)
+    assert all(segment in result_segments
+               or reverse_segment(segment) in result_segments
                # in case of cross
                or any(segment_in_segment(result_segment, segment)
                       is Relation.COMPONENT
-                      for result_segment in result.segments)
+                      for result_segment in result_segments)
                for segment in multisegment.segments
                if (segment_in_multipolygon(segment, multipolygon)
                    in (Relation.DISJOINT, Relation.CROSS)))
