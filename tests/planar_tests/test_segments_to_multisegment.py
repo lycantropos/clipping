3f2acbf6c39bcfa9ac846591d759c083a99de536
Azat Ibrakov <azatibrakov@gmail.com>
2021-05-18 10:50:19 +0300

    Complete test for 'segments_to_multisegment' function

--- tests/planar_tests/test_segments_to_multisegment.py
+++ tests/planar_tests/test_segments_to_multisegment.py
@@ -9,6 +9,8 @@ from tests.utils import (are_compounds_similar,
                          are_segments_sequences_similar,
                          equivalence,
                          is_maybe_linear,
+                         is_multisegment,
+                         is_multisegment_valid,
                          pack_non_shaped,
                          reverse_compound_coordinates,
                          reverse_segments_sequence,
@@ -25,12 +27,18 @@ def test_basic(segments: List[Segment]) -> None:
 
 
 @given(strategies.segments_lists)
+def test_validity(segments: List[Segment]) -> None:
+    result = segments_to_multisegment(segments)
+
+    assert not is_multisegment(result) or is_multisegment_valid(result)
+
+
+@given(strategies.segments_lists)
 def test_properties(segments: List[Segment]) -> None:
     result = segments_to_multisegment(segments)
 
     result_points, result_segments = pack_non_shaped(result)
     assert not result_points
-    assert not segments_cross_or_overlap(result_segments)
     assert equivalence(not segments_cross_or_overlap(segments),
                        are_segments_sequences_similar(result_segments,
                                                       segments))
