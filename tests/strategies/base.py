59415a3002ea449a7454a7f74919615d7142920c
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-15 02:23:19 +0200

    Complete strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -3,16 +3,14 @@ from functools import partial
 
 from hypothesis import strategies
 
-MAX_NUMBER = 10 ** 10
-MIN_NUMBER = -MAX_NUMBER
+MAX_SCALAR = 10 ** 10
+MIN_SCALAR = -MAX_SCALAR
 coordinates_strategies_factories = {
-    float: partial(strategies.floats,
-                   allow_nan=False,
-                   allow_infinity=False),
     Fraction: partial(strategies.fractions,
-                      max_denominator=MAX_NUMBER),
+                      max_denominator=MAX_SCALAR),
     int: strategies.integers,
 }
 coordinates_strategies = strategies.sampled_from(
-        [factory(MIN_NUMBER, MAX_NUMBER)
-         for factory in coordinates_strategies_factories.values()])
+        [factory(MIN_SCALAR, MAX_SCALAR)
+         for factory in coordinates_strategies_factories.values()]
+)
